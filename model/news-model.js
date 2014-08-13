3f401a330e9f26132df38f5644e3ffc28e3057c9
Markus Kefka's <markusdarkness@gmail.com>
2014-08-13 08:07:59 -0300

    layout news proposal

--- model/news-model.js
+++ model/news-model.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var paginate = require('mongoose-paginate');
+var slug = require('slug');
 var echoSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -13,6 +14,9 @@ var echoSchema = new mongoose.Schema({
     type: String,
     default: ''
   },
+  slug: {
+    type: String
+  },
   karma: {
     type: Number,
     default: 0
@@ -24,7 +28,22 @@ var echoSchema = new mongoose.Schema({
   created: {
     type: Date,
     default: Date.now
-  }
+  },
+  comments: [{
+    user: {
+      name: String,
+      email: String
+    },
+    created: {
+      type: Date,
+      default: Date.now
+    }
+  }],
+});
+
+echoSchema.pre('save', function(next){
+  this.slug = slug(this.title);
+  next();
 });
 
 echoSchema.plugin(paginate);
