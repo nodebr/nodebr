17ec91c7f10cdf920627afcc6ed02e1843e3c98f
Markus Kefka's <markusdarkness@gmail.com>
2014-08-18 08:55:00 -0300

    load disqus with errors

--- view/helper/news_article.js
+++ view/helper/news_article.js
@@ -1,23 +1,24 @@
 var News = require(__dirname + '/../../model/news-model');
 
-module.exports = function(chunk, context, bodies, params){
+module.exports = function(chunk, context, bodies){
   return chunk.map(function(chunk){
 
     // Verificamos o slug dá página para buscar a noticia
-    var slug = params.slug;
+    var param = context.stack.head.params;
+    var slug = param.slug;
     console.log('mark');
 
     // Fazemos a busca na Collection news em busca da noticia.
     // a noticia para ser válida tem que ter karma 0.
-    News.findOne({'slug': slug}).where('karma').gt(0).exec(function(err, news){
+    News.findOne({'slug': slug}).where('karma').gt(-1).exec(function(err, not){
       if(err)
         throw err;
 
-      console.log(news);
+      console.log(not);
 
       // Renderizamos o a noticia.
       chunk.render(bodies.block, context.push({
-        news : news,
+        news : not,
       }));
 
       // Avisamos para o dust.js que o nosso trabalho aqui terminou
