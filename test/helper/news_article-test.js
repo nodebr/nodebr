cb872a80317dbe3e01d53778316719172b2474ce
Markus Kefka's <markusdarkness@gmail.com>
2014-08-20 01:56:06 -0300

    helpers tests

---
+++ test/helper/news_article-test.js
@@ -0,0 +1,25 @@
+// Testar o helper que renderiza as notícias
+var assert = require('assert');
+var dust = require(__dirname + '/../../lib/hapi-dust');
+require(__dirname + '/../mock/loader').models();
+var dropper = require(__dirname + '/../mock/dropper');
+
+describe('Teste do helper @news_article', function(){
+
+  // Limpando o banco de dados antes e depois das operações
+  before(dropper);
+  after(dropper);
+
+  it('Deve renderizar o formulário de nova noticia', function(cb){
+    var template = '{@new_news}{/new_news}';
+    dust.compile(template, {}, function(err, compiled){
+        assert.ifError(err);
+        //context.stack.head.params
+        compiled({}, {}, function(err, html){
+          assert.ifError(err);
+          assert.ok(html.indexOf('Nova postagem à vista!!') > -1);
+          cb();
+        });
+      });
+  });
+});
--- view/helper/new_news.js
+++ view/helper/new_news.js
@@ -2,7 +2,9 @@ module.exports = function(chunk, context, bodies){
   return chunk.map(function(chunk){
 
       // Renderizamos o a noticia.
-      chunk.render(bodies.block, context.push({}));
+      chunk.render(bodies.block, context.push({
+        newPost: 'Nova postagem à vista!!',
+      }));
 
       // Avisamos para o dust.js que o nosso trabalho aqui terminou
       chunk.end();
