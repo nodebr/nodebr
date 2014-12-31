dd9ff57a006c4e2ac474aa5c8feb39ba476246fd
Jeferson Viana Perito <jefperito@gmail.com>
2014-12-31 00:49:22 -0200

    Melhorado teste

--- test/rss-test.js
+++ test/rss-test.js
@@ -1,9 +1,10 @@
 var rss = require(__dirname + '/../lib/rss');
 var assert = require('assert');
 
+
 describe('Biblioteca de geracao de RSS', function() {
 
-	it('Deve inserir um item', function() {
+	it('Deve inserir itens', function() {
 		rss.addItems({
 			title: 'Configurando o package.json',
 			link: 'http://domain.com',
@@ -21,6 +22,25 @@ describe('Biblioteca de geracao de RSS', function() {
 			            '<pubDate>Wed, 21 May 2014 15:11:00 GMT</pubDate>' +
 			        '</item>';
 
+		assert.ok(rss.getRSS().replace(/(\r\n|\n|\r)/gm,'').indexOf(xmlItem) > -1);
+
+		rss.addItems({
+			title: 'Performance em nodejs',
+			link: 'http://domain.com',
+			description: 'Dicas para melhorar a performance de suas apps',
+			user: {name: 'Peter Parker'},
+			created: new Date(2014, 01, 15, 12, 11, 0)
+		});
+
+		xmlItem = '<item>' +
+			            '<title><![CDATA[Performance em nodejs]]></title>' +
+			            '<description><![CDATA[Dicas para melhorar a performance de suas apps]]></description>' +
+			            '<link>http://domain.com</link>' +
+			            '<guid isPermaLink="true">http://domain.com</guid>' +
+			            '<dc:creator><![CDATA[Peter Parker]]></dc:creator>' +
+			            '<pubDate>Sat, 15 Feb 2014 14:11:00 GMT</pubDate>' +
+			        '</item>';
+ 
 		assert.ok(rss.getRSS().replace(/(\r\n|\n|\r)/gm,'').indexOf(xmlItem) > -1);
 	});
 });
