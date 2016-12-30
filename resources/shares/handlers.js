f50970c02e840b8cd0f490bbc35dab57b5527e11
FelipeMonobe <xinube@gmail.com>
2016-12-30 14:42:23 -0200

    catches

--- resources/shares/handlers.js
+++ resources/shares/handlers.js
@@ -1,4 +1,6 @@
+const boom = require('boom')
 const db = require('../../lib/db')
+
 const Shares = db.model('Share')
 
 exports.findAll = (req, res) =>
@@ -7,6 +9,7 @@ exports.findAll = (req, res) =>
   .orderBy('created_at')
   .fetchAll({}, { withRelated: ['user'] })
   .then(collection => res.send(collection))
+  .catch(() => { throw boom.badRequest('Ocorreu um erro na consulta') })
 
 exports.findOne = (req, res) =>
   Shares
@@ -15,6 +18,7 @@ exports.findOne = (req, res) =>
     if (!share) return res.status(404).end()
     return res.send(share)
   })
+  .catch(() => { throw boom.badRequest('Ocorreu um erro na consulta') })
 
 exports.remove = (req, res) => {
   if (req.params.id !== req.session.user_id) return res.status(403).end()
@@ -23,9 +27,11 @@ exports.remove = (req, res) => {
   .forge({ id: req.params.id })
   .destroy()
   .then(() => res.send({ success: true }))
+  .catch(() => { throw boom.badRequest('Ocorreu um erro na exclusão') })
 }
 
 exports.create = (req, res) =>
   Shares
   .create(req.body)
   .then(share => res.send(share))
+  .catch(() => { throw boom.badRequest('Ocorreu um erro na criação') })
